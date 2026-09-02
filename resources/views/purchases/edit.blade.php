14011b4aaf2d50e71dbf92264200e04b01bac407
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-09-03 00:21:10 +0500

    F

--- resources/views/purchases/edit.blade.php
+++ resources/views/purchases/edit.blade.php
@@ -71,7 +71,7 @@
                   <th width="16%">Item</th>
                   <th width="11%">Variation</th>
                   <th width="10%">Packing</th>
-                  <th width="8%">Wt./Packing (kg)</th>
+                  <th width="8%">Bag Wt.</th>
                   <th width="6%">Qty</th>
                   <th width="9%">Gross Weight</th>
                   <th width="9%">Net Weight</th>
