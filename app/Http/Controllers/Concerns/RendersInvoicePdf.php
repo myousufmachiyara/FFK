a61557b1f578d81d3080b0ae51cb7b14f3d447a5
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-09-22 21:53:39 +0500

    Update RendersInvoicePdf.php

--- app/Http/Controllers/Concerns/RendersInvoicePdf.php
+++ app/Http/Controllers/Concerns/RendersInvoicePdf.php
@@ -28,9 +28,9 @@ trait RendersInvoicePdf
      */
     protected function pdfContactIcon(\TCPDF $pdf, float $x, float $y, float $size = 9): void
     {
-        $pdf->SetFont('your_emoji_font', '', $size);
+        $pdf->SetFont('dejavusans', '', $size);
         $pdf->SetXY($x, $y);
-        $pdf->Cell(6, 5, "\u{1F4DE}", 0, 0, 'C');
+        $pdf->Cell(6, 5, "\u{260E}", 0, 0, 'C');
     }
 
     /**
