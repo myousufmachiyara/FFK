fc2868ce2cb5a70a8d4de4a4c592b58cfd61a848
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-09-23 01:37:13 +0500

    Update CommissionInvoiceController.php

--- app/Http/Controllers/CommissionInvoiceController.php
+++ app/Http/Controllers/CommissionInvoiceController.php
@@ -932,12 +932,12 @@ class CommissionInvoiceController extends Controller
 
         $vendorHtml = '<table width="100%" cellpadding="2" style="font-size:9px;">
             <tr><td width="30%"><b>Vendor</b></td><td width="5%">:</td><td width="65%">' . e($invoice->vendor->name ?? 'N/A') . '</td></tr>
-            <tr><td><b>Vendor Bill No</b></td><td>:</td><td>' . ($invoice->vendor_bill_no ?? '-') . '</td></tr>
+            <tr><td><b>Vendor Bill No</b></td><td width="5%">:</td><td>' . ($invoice->vendor_bill_no ?? '-') . '</td></tr>
             <tr><td><b>Customer</b></td><td width="5%">:</td><td width="65%">' . e($invoice->customer->name ?? 'N/A') . '</td></tr>
         </table>';
         $custHtml = '<table width="100%" cellpadding="2" style="font-size:9px;">
-            <tr><td width="10%"><b>Transport</b></td><td>:</td><td>' . ($invoice->transport_name ?? '-') . '</td></tr>
-            <tr><td width="10%"><b>Bilti No</b></td><td>:</td><td>' . ($invoice->bilty_no ?? '-') . '</td></tr>
+            <tr><td width="30%"><b>Transport:</b></td><td width="5%">:</td><td>' . ($invoice->transport_name ?? '-') . '</td></tr>
+            <tr><td width="30%"><b>Bilti No:</b></td><td width="5%">:</td><td>' . ($invoice->bilty_no ?? '-') . '</td></tr>
         </table>';
 
         $pdf->SetXY(10, $boxY + 7);
