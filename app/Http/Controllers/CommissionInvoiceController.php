0ce3bc8bef742a14df42d2191e8e246259dabcbb
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-09-23 01:57:29 +0500

    Update CommissionInvoiceController.php

--- app/Http/Controllers/CommissionInvoiceController.php
+++ app/Http/Controllers/CommissionInvoiceController.php
@@ -955,8 +955,8 @@ class CommissionInvoiceController extends Controller
                     <th width="16%">Item</th><th width="5%">Qty</th><th width="7%">G.Wt</th><th width="7%">N.Wt</th>
                     <th width="8%">P.Rate/kg</th><th width="10%">P.Total</th>
                     <th width="8%">S.Rate/kg</th><th width="10%">S.Total</th>
-                    <th width="5%">V.C %</th><th width="9%">V.C</th>
-                    <th width="5%">C.C %</th><th width="9%">C.C</th>
+                    <th width="5%">V.C %</th><th width="9.5%">V.C</th>
+                    <th width="5%">C.C %</th><th width="9.5%">C.C</th>
                 </tr>
             </thead>
             <tbody>';
@@ -974,9 +974,9 @@ class CommissionInvoiceController extends Controller
                     <td width="8%" style="text-align:right;">' . number_format($item->sale_price, 2) . '</td>
                     <td width="10%" style="text-align:right;">' . number_format($item->sale_total, 2) . '</td>
                     <td width="5%" style="text-align:center;">' . number_format($item->vendor_commission_percentage, 2) . '</td>
-                    <td width="9%" style="text-align:right;">' . number_format($item->vendor_commission_amount, 2) . '</td>
+                    <td width="9.5%" style="text-align:right;">' . number_format($item->vendor_commission_amount, 2) . '</td>
                     <td width="5%" style="text-align:center;">' . number_format($item->customer_commission_percentage, 2) . '</td>
-                    <td width="9%" style="text-align:right;">' . number_format($item->customer_commission_amount, 2) . '</td>
+                    <td width="9.5%" style="text-align:right;">' . number_format($item->customer_commission_amount, 2) . '</td>
                 </tr>';
         }
         $html .= '</tbody></table>';
