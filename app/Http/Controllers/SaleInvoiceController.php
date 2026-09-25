b03773a73659059ceb26597be421ef9fe28cbc77
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-09-26 00:26:05 +0500

    Update SaleInvoiceController.php

--- app/Http/Controllers/SaleInvoiceController.php
+++ app/Http/Controllers/SaleInvoiceController.php
@@ -730,17 +730,17 @@ class SaleInvoiceController extends Controller
 
         $expRows = '';
         foreach ($invoice->expenses as $exp) {
-            $expRows .= '<tr><td width="65%">' . $exp->typeLabel() . '</td><td width="35%" style="text-align:right;">' . number_format($exp->amount, 2) . '</td></tr>';
+            $expRows .= '<tr><td width="25%">' . $exp->typeLabel() . '</td><td width="50%">' . $exp->description . '</td><td width="25%" style="text-align:right;">' . number_format($exp->amount, 2) . '</td></tr>';
         }
         if (!$invoice->expenses->count()) {
-            $expRows = '<tr><td colspan="2" style="color:#888;">No Other Expenses</td></tr>';
+            $expRows = '<tr><td colspan="3" style="color:#888;">No Other Expenses</td></tr>';
         }
 
         $leftHtml = '
         <table width="100%" cellpadding="2" style="font-size:9px;">
-            <tr style="background-color:#1B3A5C;color:#ffffff;font-weight:bold;"><td colspan="2">  Additional Information — Expenses</td></tr>
+            <tr style="background-color:#1B3A5C;color:#ffffff;font-weight:bold;"><td colspan="3">  Additional Information — Expenses</td></tr>
             ' . $expRows . '
-            <tr style="font-weight:bold;background-color:#F5EFDF;"><td>Total Expenses</td><td style="text-align:right;">' . number_format($invoice->total_other_expenses, 2) . '</td></tr>
+            <tr style="font-weight:bold;background-color:#F5EFDF;"><td width="30%">Total Expenses</td><td width="70%" colspan="2" style="text-align:right;">' . number_format($invoice->total_other_expenses, 2) . '</td></tr>
         </table>';
 
         $rightRows = '
@@ -871,17 +871,17 @@ class SaleInvoiceController extends Controller
 
         $expRows = '';
         foreach ($invoice->expenses as $exp) {
-            $expRows .= '<tr><td width="65%">' . $exp->typeLabel() . '</td><td width="35%" style="text-align:right;">' . number_format($exp->amount, 2) . '</td></tr>';
+            $expRows .= '<tr><td width="25%">' . $exp->typeLabel() . '</td><td width="50%">' . $exp->description . '</td><td width="25%" style="text-align:right;">' . number_format($exp->amount, 2) . '</td></tr>';
         }
         if (!$invoice->expenses->count()) {
-            $expRows = '<tr><td colspan="2" style="color:#888;">No Other Expenses</td></tr>';
+            $expRows = '<tr><td colspan="3" style="color:#888;">No Other Expenses</td></tr>';
         }
 
         $leftHtml = '
         <table width="100%" cellpadding="2" style="font-size:9px;">
-            <tr style="background-color:#1B3A5C;color:#ffffff;font-weight:bold;"><td colspan="2">  Additional Information — Expenses</td></tr>
+            <tr style="background-color:#1B3A5C;color:#ffffff;font-weight:bold;"><td colspan="3">  Additional Information — Expenses</td></tr>
             ' . $expRows . '
-            <tr style="font-weight:bold;background-color:#F5EFDF;"><td>Total Expenses</td><td style="text-align:right;">' . number_format($invoice->total_other_expenses, 2) . '</td></tr>
+            <tr style="font-weight:bold;background-color:#F5EFDF;"><td width="30%">Total Expenses</td><td width="70%" colspan="2" style="text-align:right;">' . number_format($invoice->total_other_expenses, 2) . '</td></tr>
         </table>';
 
         $rightRows = '
